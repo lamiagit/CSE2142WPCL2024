006d3904fff3c65f621f50c4fcc6de795900735c
lamiagit <lamiakawsarst@gmail.com>
2025-11-21 04:44:16 +0600

    Task 7: Used straightforward option for search operation

--- src/EmployeeManager.java
+++ src/EmployeeManager.java
@@ -4,27 +4,27 @@ import java.util.*;
 
 public class EmployeeManager {
 
-    private static String[] readEmployees() throws IOException { //change made 6
+    private static String[] readEmployees() throws IOException {
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(
-                        new FileInputStream(Constants.EMPLOYEE_FILE))); //change made 6
-        String[] employees = reader.readLine().split(","); //change made 6
-        reader.close(); //change made 6
-        return employees; //change made 6
+                        new FileInputStream(Constants.EMPLOYEE_FILE)));
+        String[] employees = reader.readLine().split(",");
+        reader.close();
+        return employees;
     }
 
-    private static void writeEmployees(String[] employees) throws IOException { //change made 6
+    private static void writeEmployees(String[] employees) throws IOException {
         BufferedWriter writer = new BufferedWriter(
-                new FileWriter(Constants.EMPLOYEE_FILE)); //change made 6
-        writer.write(String.join(",", employees)); //change made 6
-        writer.close(); //change made 6
+                new FileWriter(Constants.EMPLOYEE_FILE));
+        writer.write(String.join(",", employees));
+        writer.close();
     }
 
-    private static void appendEmployee(String employee) throws IOException { //change made 6
+    private static void appendEmployee(String employee) throws IOException {
         BufferedWriter writer = new BufferedWriter(
-                new FileWriter(Constants.EMPLOYEE_FILE, true)); //change made 6
-        writer.write(", " + employee); //change made 6
-        writer.close(); //change made 6
+                new FileWriter(Constants.EMPLOYEE_FILE, true));
+        writer.write(", " + employee);
+        writer.close();
     }
 
     public static void main(String[] args) {
@@ -36,7 +36,7 @@ public class EmployeeManager {
         if (args[0].equals("l")) {
             System.out.println(Constants.LOADING_MESSAGE);
             try {
-                for (String employee : readEmployees()) { //change made 6
+                for (String employee : readEmployees()) {
                     System.out.println(employee);
                 }
             } catch (Exception ex) {}
@@ -44,7 +44,7 @@ public class EmployeeManager {
         } else if (args[0].equals("s")) {
             System.out.println(Constants.LOADING_MESSAGE);
             try {
-                String[] employees = readEmployees(); //change made 6
+                String[] employees = readEmployees();
                 System.out.println(String.join(",", employees));
                 System.out.println(employees[new Random().nextInt(employees.length)]);
             } catch (Exception ex) {}
@@ -52,25 +52,28 @@ public class EmployeeManager {
         } else if (args[0].contains("+")) {
             System.out.println(Constants.LOADING_MESSAGE);
             try {
-                appendEmployee(args[0].substring(1)); //change made 6
+                appendEmployee(args[0].substring(1));
             } catch (Exception ex) {}
             System.out.println(Constants.DATA_LOADED);
         } else if (args[0].contains("?")) {
             System.out.println(Constants.LOADING_MESSAGE);
             try {
-                String searchName = args[0].substring(1);
-                for (String employee : readEmployees()) { //change made 6
-                    if (employee.equals(searchName)) {
-                        System.out.println("Employee found!");
-                        break;
-                    }
-                }
+                String searchName = args[0].substring(1); //change made 7
+                boolean found = Arrays.stream(readEmployees()) //change made 7
+                        .anyMatch(employee -> { //change made 7
+                            if (employee.equals(searchName)) { //change made 7
+                                System.out.println("Employee found!"); //change made 7
+                                return true; //change made 7
+                            }
+                            return false; //change made 7
+                        }); //change made 7
+                if (!found) System.out.println("Employee not found!"); //change made 7
             } catch (Exception ex) {}
             System.out.println(Constants.DATA_LOADED);
         } else if (args[0].contains("c")) {
             System.out.println(Constants.LOADING_MESSAGE);
             try {
-                char[] characters = String.join(",", readEmployees()).toCharArray(); //change made 6
+                char[] characters = String.join(",", readEmployees()).toCharArray();
                 boolean inWord = false;
                 int wordCount = 0;
                 for (char character : characters) {
@@ -90,22 +93,22 @@ public class EmployeeManager {
             System.out.println(Constants.LOADING_MESSAGE);
             try {
                 String updateName = args[0].substring(1);
-                String[] employees = readEmployees(); //change made 6
+                String[] employees = readEmployees();
                 for (int i = 0; i < employees.length; i++) {
                     if (employees[i].equals(updateName)) {
                         employees[i] = "Updated";
                     }
                 }
-                writeEmployees(employees); //change made 6
+                writeEmployees(employees);
             } catch (Exception ex) {}
             System.out.println(Constants.DATA_UPDATED);
         } else if (args[0].contains("d")) {
             System.out.println(Constants.LOADING_MESSAGE);
             try {
                 String deleteName = args[0].substring(1);
-                List<String> employeeList = new ArrayList<>(Arrays.asList(readEmployees())); //change made 6
+                List<String> employeeList = new ArrayList<>(Arrays.asList(readEmployees()));
                 employeeList.remove(deleteName);
-                writeEmployees(employeeList.toArray(new String[0])); //change made 6
+                writeEmployees(employeeList.toArray(new String[0]));
             } catch (Exception ex) {}
             System.out.println(Constants.DATA_DELETED);
         }
